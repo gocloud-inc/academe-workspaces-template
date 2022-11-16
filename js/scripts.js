ded8e5d753ad28617c727214cc31d5ea6acf3771
GitMarkPaul <markpaulcaneda1997@gmail.com>
2022-11-16 13:46:36 +0800

    ui updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,40 @@ document.addEventListener('DOMContentLoaded', function() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-tooltip="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    let passwordEl = document.getElementById('password');
-    let passwordBtn = document.getElementById('password-btn');
-    let passwordIcon = document.getElementById('password-icon');
+    let passwordEl = document.querySelectorAll('.password');
+    let passwordBtn = document.querySelectorAll('.password-btn');
+    let passwordIcon = document.querySelectorAll('.password-icon');
 
     if (passwordBtn) {
-        passwordBtn.addEventListener('click', () => {
-            if (passwordEl.type === 'password') {
-                passwordEl.type = 'text';
-                passwordIcon.innerHTML = 'visibility_off';
-            } else {
-                passwordEl.type = 'password';
-                passwordIcon.innerHTML = 'visibility';
-            }
-        });
+        passwordBtn.forEach((index) => {
+            index.addEventListener('click', () => {
+                passwordEl.forEach((passwordIndex) => {
+                    if (passwordIndex.type === 'password') {
+                        passwordIndex.type = 'text';
+                        passwordIcon.forEach((passwordIconIndex) => {
+                            passwordIconIndex.innerHTML = 'visibility_off';
+                        });
+                    } else {
+                        passwordIndex.type = 'password';
+                        passwordIcon.forEach((passwordIconIndex) => {
+                            passwordIconIndex.innerHTML = 'visibility';
+                        });
+                    }
+                });
+            });
+        })
     }
 
-    let formEl = document.getElementById('form');
-    let clearFormBtn = document.getElementById('clear-form');
-
+    let formEl = document.querySelectorAll('.form');
+    let clearFormBtn = document.querySelectorAll('.clear-form');
+    
     if (clearFormBtn) {
-        clearFormBtn.addEventListener('click',  () => {
-            formEl.reset();
+        clearFormBtn.forEach((index) => {
+            index.addEventListener('click',  () => {
+                formEl.forEach((formIndex) => {
+                    formIndex.reset();
+                });
+            });
         });
     }
 });
